0eb5fc67ac62926dc456f13947c54782b9cc56bc
Oliver Scott <olivercscott@gmail.com>
2023-04-10 12:35:40 -0400

    base: Add support for hide applist [1/2]
    Author: Wang Han <wanghan1995315@gmail.com>
    Date:   Tue Aug 29 15:21:14 2023 +0000
    
    Change-Id: I8caa9030f4ff44babda5a778d0e0d6849e1e4d1b
    Signed-off-by: someone5678 <59456192+someone5678@users.noreply.github.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -2791,6 +2791,8 @@ public final class SystemServer implements Dumpable {
             t.traceBegin("StartHealthService");
             mSystemServiceManager.startService(HealthInterfaceService.class);
             t.traceEnd();
+
+            mSystemServiceManager.startService(HideAppListService.class);
         }
 
         t.traceBegin("StartMediaProjectionManager");
